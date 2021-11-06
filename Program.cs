21f755f4f26542752c1b80082a54816b91d93c90
Grace Tveit <grace.tveit@gmail.com>
2021-11-06 14:07:43 -0700

    feat: wrote FindFiles to search fileSystem

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace files_module
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var salesFiles = FindFiles("stores");
+
+      foreach (var file in salesFiles)
+      {
+        Console.WriteLine(file);
+      }
     }
+
+    static IEnumerable<string> FindFiles(string folderName)
+    {
+      List<string> salesFiles = new List<string>();
+
+      var foundFiles = Directory.EnumerateFiles(
+        folderName,
+        "*",
+        SearchOption.AllDirectories
+      );
+
+      foreach (var file in foundFiles)
+      {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+          salesFiles.Add(file);
+        }
+      }
+
+      return salesFiles;
+    }
+  }
 }
